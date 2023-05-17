02ff88852f2e2541c65b7dfb1003cb0e63a02f4c
Hrusikesh <hrusikesh9999@gmail.com>
2023-05-18 03:21:45 +0530

    Added proper scrolling to all pages with details

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.container_check_floor);
+        setContentView(R.layout.movement_slip_issue);
     }
 }
