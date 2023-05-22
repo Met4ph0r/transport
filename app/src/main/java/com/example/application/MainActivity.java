f29aff13276238efa2458a6b8e9c7dd828d31daf
Hrusikesh <hrusikesh9999@gmail.com>
2023-05-22 19:40:26 +0530

    Fleet Owner new Screens

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.depot_screen_02__issue_movement_slip);
+        setContentView(R.layout.fleet_owner_screeen_1__homescreen);
     }
 }
