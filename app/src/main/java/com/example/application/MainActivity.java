44ae28b94bc17e5a7d7153369b969d7892abae59
Hrusikesh <hrusikesh9999@gmail.com>
2023-06-02 21:12:01 +0530

    Adjusted padding in customer screens. All 6 customer screens done.

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.customer_screen_01__start_booking);
+        setContentView(R.layout.customer_screen_05__tracking_shipment);
 
 //        String apiKey = getString(R.string.api_key);
 //        if (!Places.isInitialized()) {
